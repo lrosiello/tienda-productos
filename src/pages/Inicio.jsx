55ecff2b3d2f7e03b51dd25eb4983d5ce551c686
lrosiello <lrosiello@alumnos.exa.unicen.edu.ar>
2023-05-10 18:53:47 -0300

    Algunos retoques

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import { Grid,Typography } from "@mui/material";
-import Productos from "./Productos"
-import ContactoForm from "../components/ContactoForm"
+import { Grid, Typography } from "@mui/material";
+import Productos from "./Productos";
+import ContactoForm from "../components/ContactoForm";
+import backgroundImage from "../assets/cartelera.jpg"
 
 export default function Inicio() {
   return (
     <>
       {/*------------------CARTELERA-------------------------*/}
-      <Grid item md={12} xs={12} p={5}>
-            <Typography variant="h5" color="initial" textAlign="center">
-              TODO LO QUE NECESITAS, LO PODES ENCONTRAR ACA
-            </Typography>
-          </Grid>
-      <Productos/>
-      <ContactoForm/>
+      <Grid
+        item
+        md={12}
+        xs={12}
+        p={5}
+        style={{
+          backgroundImage: backgroundImage,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <Typography variant="h5" color="initial" textAlign="center" style={{ color: "#fff" }}>
+          TODO LO QUE NECESITAS, LO PODES ENCONTRAR ACA
+        </Typography>
+      </Grid>
+      <Productos />
+      <ContactoForm />
     </>
   );
 }
--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,26 +1,37 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Paper, Box } from "@mui/material";
+import imagen from "../assets/image.jpg";
+
 export default function Productos() {
+
+
+  const arrayProductos = () => {
+    const productos = [];
+    for (let index = 0; index < 12; index++) {
+      productos.push({
+        title: "Producto " + (index + 1),
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem odio ea consectetur perspiciatis magnam, dolores tenetur aut, adipisci aspernatur eaque quisquam architecto nulla officia voluptate a labore illo reprehenderit corrupti.",
+        image:imagen
+          
+      });
+    }
+    return productos;
+  };
+
   return (
     <>
-      <Grid item md={4} xs={12} p={5}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
-        nesciunt numquam doloribus perspiciatis fugit ut, ipsa error accusantium
-        repellat tempora. Tempore quis nulla asperiores esse iste distinctio
-        doloremque commodi dolorem.
-      </Grid>
-      <Grid item md={4} xs={12} p={5}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
-        nesciunt numquam doloribus perspiciatis fugit ut, ipsa error accusantium
-        repellat tempora. Tempore quis nulla asperiores esse iste distinctio
-        doloremque commodi dolorem.
-      </Grid>
-      <Grid item md={4} xs={12} p={5}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
-        nesciunt numquam doloribus perspiciatis fugit ut, ipsa error accusantium
-        repellat tempora. Tempore quis nulla asperiores esse iste distinctio
-        doloremque commodi dolorem.
-      </Grid>
+      {arrayProductos().map((producto, index) => (
+        <Grid item key={index} md={3} xs={12} p={5}>
+          <Box height={500} maxWidth={400}>
+            <Paper elevation={6} sx={{ height: "100%", padding: "16px"  }}>
+              <img src={producto.image} alt={producto.title} style={{ width: "100%", height: "auto" }} />
+              <h3>{producto.title}</h3>
+              <p>{producto.description}</p>
+            </Paper>
+          </Box>
+        </Grid>
+      ))}
     </>
   );
 }
